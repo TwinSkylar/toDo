5a6d2dba079b64351395f8f435ec837025f47648
John Luu <122046486+TwinSkylar@users.noreply.github.com>
2023-05-18 15:24:50 -0400

    Added input fields to the modal

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -1,12 +1,17 @@
 export default class customModal {
-  constructor({ titleText, descriptionText,expDateText,priorityText,notesText}) {
+  constructor({
+    titleText,
+    descriptionText,
+    expDateText,
+    priorityText,
+    notesText,
+  }) {
     this.titleText = titleText;
     this.descriptionText = descriptionText;
     this.expDateText = expDateText;
     this.priorityText = priorityText;
     this.notesText = notesText;
   }
-
 
   createAndOpen(onConfirm, onCancel) {
     this.modalElem = document.createElement("div");
@@ -21,30 +26,101 @@ export default class customModal {
     modalContentElem.classList.add("content");
     this.modalElem.appendChild(modalContentElem);
 
-    const titleTextElem = document.createElement("p");
+    const titleTextElem = document.createElement("div");
     titleTextElem.classList.add("titleText");
+    //Label
+    const titleTextLabel = document.createElement("label");
+    titleTextLabel.className = "title";
     titleTextElem.textContent = this.titleText;
-    console.log ("hit should display: " + this + " and " + this.titleText);
+    titleTextElem.appendChild(titleTextLabel);
+    //Input
+    const titleTextInput = document.createElement("input");
+    titleTextInput.id = "title";
+    titleTextInput.className = "title";
+    titleTextElem.appendChild(titleTextInput);
 
     modalContentElem.appendChild(titleTextElem);
 
-    const dateTextElem = document.createElement("p");
-    dateTextElem.classList.add("expDateText");
-    dateTextElem.textContent = this.expDateText;
-
-    modalContentElem.appendChild(dateTextElem);
-
-    const descriptionTextElem = document.createElement("p");
-    descriptionTextElem.classList.add("descriptionText");
-    descriptionTextElem.textContent = this.descriptionText;
-
-    modalContentElem.appendChild(descriptionTextElem);
-
-    const priorityTextElem = document.createElement("p");
+    //Container div
+    const priorityTextElem = document.createElement("div");
     priorityTextElem.classList.add("priorityText");
-    priorityTextElem.textContent = this.priorityText;
+    const sectionTitle = document.createElement("p");
+    sectionTitle.textContent="Priority";
+    modalContentElem.appendChild(sectionTitle);
+    //Label
+    const priorityLowTextLabel = document.createElement("label");
+    priorityLowTextLabel.for = "low";
+    priorityLowTextLabel.textContent = "Low";
+    priorityTextElem.appendChild(priorityLowTextLabel);
+    //Input
+    const priorityLowTextInput = document.createElement("input");
+    priorityLowTextInput.id = "priority";
+    priorityLowTextInput.className = "priority";
+    priorityLowTextInput.type = "radio";
+    priorityLowTextInput.value = "low";
+    priorityTextElem.appendChild(priorityLowTextInput);
+    //Container div
+
+    //Label
+    const priorityMedTextLabel = document.createElement("label");
+    priorityMedTextLabel.for = "med";
+    priorityMedTextLabel.textContent = "Med";
+    priorityTextElem.appendChild(priorityMedTextLabel);
+    //Input
+    const priorityMedTextInput = document.createElement("input");
+    priorityMedTextInput.id = "priority";
+    priorityMedTextInput.className = "priority";
+    priorityMedTextInput.type = "radio";
+    priorityMedTextInput.value = "med";
+    priorityTextElem.appendChild(priorityMedTextInput);
+
+
+    //Label
+    const priorityHighTextLabel = document.createElement("label");
+    priorityHighTextLabel.for = "high";
+    priorityHighTextLabel.textContent = "High";
+    priorityTextElem.appendChild(priorityHighTextLabel);
+    //Input
+    const priorityHighTextInput = document.createElement("input");
+    priorityHighTextInput.id = "priority";
+    priorityHighTextInput.className = "priority";
+    priorityHighTextInput.type = "radio";
+    priorityHighTextInput.value = "high";
+    priorityTextElem.appendChild(priorityHighTextInput);
+
     modalContentElem.appendChild(priorityTextElem);
 
+    //Container div
+    const expDateTextElem = document.createElement("div");
+    expDateTextElem.classList.add("expDateText");
+    //Label
+    const expDateTextLabel = document.createElement("label");
+    expDateTextLabel.className = "expDate";
+    expDateTextElem.textContent = 'Due Date:';
+    expDateTextElem.appendChild(expDateTextLabel);
+    //Input
+    const expDateTextInput = document.createElement("input");
+    expDateTextInput.id = "expDate";
+    expDateTextInput.className = "expDate";
+    expDateTextInput.type = "date";
+    expDateTextElem.appendChild(expDateTextInput);
+
+    modalContentElem.appendChild(expDateTextElem);
+
+    const descriptionTextElem = document.createElement("div");
+    descriptionTextElem.classList.add("descriptionText");
+    //Label
+    const descriptionTextLabel = document.createElement("label");
+    descriptionTextLabel.className = "description";
+    descriptionTextElem.textContent = "Description:";
+    descriptionTextElem.appendChild(descriptionTextLabel);
+    //Input
+    const descriptionTextInput = document.createElement("input");
+    descriptionTextInput.id = "description";
+    descriptionTextInput.className = "description";
+    descriptionTextElem.appendChild(descriptionTextInput);
+
+    modalContentElem.appendChild(descriptionTextElem);
 
     const cancelBtnElem = document.createElement("button");
     cancelBtnElem.classList.add("cancelBtnText");
