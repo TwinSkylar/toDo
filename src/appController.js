b532fa11a8d0eac1c2a451ade5edb664463728bd
John Luu <122046486+TwinSkylar@users.noreply.github.com>
2023-05-19 13:55:45 -0400

    Changed the color pallete of the website

--- src/appController.js
+++ src/appController.js
@@ -1,6 +1,6 @@
 import projects from "./projects.js";
-import toDo from './todos.js';
-import {ScreenController,displayProject} from './UIController.js';
+import toDo from "./todos.js";
+import { ScreenController, displayProject } from "./UIController.js";
 
 const allProjects = [];
 
@@ -8,39 +8,56 @@ export function populateStorage() {
   /*test cases*/
   let dataArray = [];
 
-  dataArray.push(new toDo("title1", "description 1",  randomDate(), "low", "notes"));
-  dataArray.push(new toDo("title2", "description 2",  randomDate(), "low", "notes"));
-  dataArray.push(new toDo("title3", "description 3",  randomDate(), "medium", "notes"));
-  dataArray.push(new toDo("title4", "description 4",  randomDate(), "high", "notes"));
-  dataArray.push(new toDo("title5", "description 5",  randomDate(), "medium", "notes"));
-  dataArray.push(new toDo("title7", "description 6",  randomDate(), "high", "notes"));
-
+  dataArray.push(
+    new toDo("title1", "description 1", randomDate(), "low", "notes")
+  );
+  dataArray.push(
+    new toDo("title2", "description 2", randomDate(), "low", "notes")
+  );
+  dataArray.push(
+    new toDo("title3", "description 3", randomDate(), "medium", "notes")
+  );
+  dataArray.push(
+    new toDo("title4", "description 4", randomDate(), "high", "notes")
+  );
+  dataArray.push(
+    new toDo("title5", "description 5", randomDate(), "medium", "notes")
+  );
+  dataArray.push(
+    new toDo("title7", "description 6", randomDate(), "high", "notes")
+  );
 
   //const list = new taskList();
-  const project1 = new projects('first project');
-  const project2 = new projects('second project');
+  const project1 = new projects("first project");
+  const project2 = new projects("second project");
   dataArray.forEach((element) => {
     project1.addTask(element);
   });
 
   dataArray = [];
-  dataArray.push(new toDo("proj1", "description 1", randomDate(), "high", "notes"));
-  dataArray.push(new toDo("proj2", "description 2", randomDate(), "high", "notes"));
-  dataArray.push(new toDo("proj3", "description 3", randomDate(), "low", "notes"));
+  dataArray.push(
+    new toDo("proj1", "description 1", randomDate(), "high", "notes")
+  );
+  dataArray.push(
+    new toDo("proj2", "description 2", randomDate(), "high", "notes")
+  );
+  dataArray.push(
+    new toDo("proj3", "description 3", randomDate(), "low", "notes")
+  );
 
   dataArray.forEach((element) => {
     project2.addTask(element);
   });
-  
-  addProject (project1);
-  addProject (project2);
+
+  addProject(project1);
+  addProject(project2);
 
   ScreenController(allProjects);
   displayProject(project1);
 }
 
-export function addProject (project){
-    allProjects.push(project);
+export function addProject(project) {
+  allProjects.push(project);
 }
 
 function randomDate() {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import taskList from './taskList.js';
 import {ScreenController} from './UIController.js';
 import css from './styles.css';
 import {populateStorage} from './appController.js';
-import {isDate} from 'date-fns';
 
 populateStorage();
